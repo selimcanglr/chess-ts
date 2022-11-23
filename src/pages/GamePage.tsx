cf6584fbd39d13a1c017d50e36238977795d82c2
Selim Can Guler <selimcangler@gmail.com>
2022-11-23 22:14:02 +0300

    initial

---
+++ src/pages/GamePage.tsx
@@ -0,0 +1,7 @@
+const GamePage = () => {
+    return (
+        
+    );
+}
+ 
+export default GamePage;
---
+++ src/pages/StartPage.tsx
@@ -0,0 +1,7 @@
+const StartPage = () => {
+    return (
+        
+    );
+}
+ 
+export default StartPage;
